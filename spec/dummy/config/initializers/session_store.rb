cf646943fc8086e5a1594ebb686c4129f96500d3
moro <kyosuke-morohashi@cookpad.com>
2016-03-18 11:05:23 +0900

    Clean code

--- spec/dummy/config/initializers/session_store.rb
+++ spec/dummy/config/initializers/session_store.rb
@@ -2,7 +2,7 @@ Rails.application.config.session_store SessionStoreMigrator::CookieToRedis, {
   source: {key: '_dummy_session'},
   destination: {
     key_prefix: 'dummy_session:',
-    redis: {client: SessionStoreMigratorTestHelper.redis},
+    redis: { client: SessionStoreMigratorTestHelper.redis },
     serializer: :json,
   }
 }
